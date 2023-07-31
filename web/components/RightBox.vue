27f2e83ace9a2b0997aad3afd132a9127ef27263
dashixiong47 <475896756@qq.com>
2023-07-31 23:34:17 +0800

    主体响应式

---
+++ web/components/RightBox.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gray-500 ">12112312</div>
+</template>
+
+<script setup></script>
---
+++ web/components/Search.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-2/6 h-full border-2 flex items-center rounded-3xl px-3">
+    <Icon class="w-6 text-gray-200" name="icon-sousuo" size="text-xl mx-1"></Icon>
+    <input class="block w-full bg-inherit focus:border-none focus:outline-none" placeholder="Start typing to search.." type="text" />
+  </div>
+</template>
+
+<script setup></script>
